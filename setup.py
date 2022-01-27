5f452447917ac7bdd5cdde41c3c296259f805db4
bobturneruk <r.d.turner@sheffield.ac.uk>
2022-01-27 10:26:09 +0000

    fix dependencies

--- setup.py
+++ setup.py
@@ -3,5 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='Curves',
     version='0.1.0',
-    packages=find_packages(include=['pandas', 'numpy'])
+    packages=find_packages(),
+    install_requires=['numpy',
+                      'pandas',]
 )
