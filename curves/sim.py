332fa7916c7427d9afa5667a7fcf6aad97d27fbd
bobturneruk <r.d.turner@sheffield.ac.uk>
2022-01-27 10:18:00 +0000

    need n points

--- curves/sim.py
+++ curves/sim.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-def circle(radius: float) -> pd.DataFrame:
+def circle(radius: float, n_points: int) -> pd.DataFrame:
     # Returns a dataframe with columns `x` and `y`
     # These are the co-ordinates of points on a circle of
-    # specified radius
+    # specified radius, with n_points evenly spaced
     pass
