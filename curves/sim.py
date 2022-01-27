1785c8b50444053b66d222ae510b732ee46f67db
Jean <mdu12@sheffield.ac.uk>
2022-01-27 17:20:48 +0000

    Simulation of a circle

--- curves/sim.py
+++ curves/sim.py
@@ -4,4 +4,16 @@ def circle(radius: float, n_points: int) -> pd.DataFrame:
     # Returns a dataframe with columns `x` and `y`
     # These are the co-ordinates of points on a circle of
     # specified radius, with n_points evenly spaced
-    pass
+    dfcircle = pd.DataFrame(index=range(0, n_points * 2))
+    for i in range(0, n_points):
+        x = -radius + i * radius * 2 / n_points
+        y = (radius ** 2 - x ** 2) ** 0.5
+        dfcircle.at[i, 'x'] = x
+        dfcircle.at[i, 'y'] = y
+    for i in range(n_points, n_points*2):
+        x = radius - (i - n_points) * radius * 2 / n_points
+        y = (radius ** 2 - x ** 2) ** 0.5
+        dfcircle.at[i, 'x'] = x
+        dfcircle.at[i, 'y'] = y
+
+    return dfcircle
