4c0901e36f3483fcd201901aeda74de3d830c793
Jean <mdu12@sheffield.ac.uk>
2022-02-15 12:45:08 +0000

    Updating the test

--- tests/test_measure.py
+++ tests/test_measure.py
@@ -1,9 +1,13 @@
 import curves.measure as measure
 import curves.sim as sim
+import numpy as np
 
 def test_curvature(test_curve, test_radius):
     # Check that the right radius of curvature
     # is returned
     curvature = measure.curvature(test_curve, closed=True)
+    for index, row in curvature.iterrows():
+        print(row['Curvature'])
+        np.testing.assert_allclose(row['Curvature'], 1/test_radius, 0.2)
 
 
